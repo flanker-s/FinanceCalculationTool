e22a917a040fe41f69a4c10062d898c3b8b5c13e
Mohavk <mohavkv12@gmail.com>
2022-03-10 17:44:09 +0200

    Add. MySQL DB for testing

--- admin_api/config/database.php
+++ admin_api/config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'admin_db'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'admin_db'),
             'username' => env('DB_USERNAME', 'root'),
@@ -63,6 +63,26 @@ return [
             ]) : [],
         ],
 
+        'mysql_test' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST_TEST', 'admin_db_test'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'admin_db_test'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'uy%6#kdS)@9'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+        
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
