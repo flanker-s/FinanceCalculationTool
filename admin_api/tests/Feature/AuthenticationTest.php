f2dbc7677fb02b3a6222d81a6183ac6dcdf6b1eb
Mohavk <mohavkv12@gmail.com>
2022-03-19 20:41:38 +0200

    Chore. AuthenticationTest phpDocs

--- admin_api/tests/Feature/AuthenticationTest.php
+++ admin_api/tests/Feature/AuthenticationTest.php
@@ -10,7 +10,7 @@ class AuthenticationTest extends TestCase
 {
     use RefreshDatabase;
     /**
-     * A basic feature test example.
+     * Test if user can register
      *
      * @return void
      */
@@ -24,7 +24,11 @@ class AuthenticationTest extends TestCase
 
         $response->assertStatus(201);
     }
-
+    /**
+     * Test if user can login
+     *
+     * @return void
+     */
     public function test_user_can_get_an_access_token(){
         $password = 'password';
         $factory = User::factory(['password' => bcrypt($password)]);
