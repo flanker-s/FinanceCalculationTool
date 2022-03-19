11068708376a610faeefd090c62c150095cc7bb2
Mohavk <mohavkv12@gmail.com>
2022-03-19 12:43:50 +0200

    Change. Register & Login route path

--- admin_api/routes/api.php
+++ admin_api/routes/api.php
@@ -24,8 +24,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/auth/register', [AuthController::class, 'register'])->name('register');
-Route::post('/auth/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::group([
     'prefix' => 'v1',
