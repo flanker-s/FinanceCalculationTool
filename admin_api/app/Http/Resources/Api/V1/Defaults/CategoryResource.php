0e4c28ac1a72c5313a20f79de9ca54d5a2a61f34
Mohavk <mohavkv12@gmail.com>
2022-04-06 22:00:26 +0200

    Change. Category Json structure to same as in Tests

--- admin_api/app/Http/Resources/Api/V1/Defaults/CategoryResource.php
+++ admin_api/app/Http/Resources/Api/V1/Defaults/CategoryResource.php
@@ -3,23 +3,30 @@
 namespace App\Http\Resources\Api\V1\Defaults;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\CustomPackages\ResourceAdvancedCodnitions\Traits\HasAdvancedConditions;
 
 class CategoryResource extends JsonResource
 {
+    use HasAdvancedConditions;
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
         return [
+            'type' => 'categories',
             'id' => $this->id,
-            'name' => $this->name,
-            'operation' => $this->operation,
-            'created_at' => $this->created_at,
-            'templates' => TemplateResource::collection($this->whenLoaded('templates'))
+            'attributes' => [
+                'name' => $this->name,
+                'created_at' => $this->created_at
+            ],
+            'included' => $this->whenAnyLoaded([
+                'operation' => new OperationResource($this->whenLoaded('operation')),
+                'templates' => TemplateResource::collection($this->whenLoaded('templates')),
+            ]),
         ];
     }
 }
