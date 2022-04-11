41714e0b9b89aeff8ee32bc1f550af1f4907b80c
Mohavk <mohavkv12@gmail.com>
2022-04-11 19:23:18 +0200

    Add. Operation request validation

--- admin_api/app/Http/Controllers/Api/V1/Defaults/OperationController.php
+++ admin_api/app/Http/Controllers/Api/V1/Defaults/OperationController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api\V1\Defaults;
 
 use App\CustomPackages\QueryRequest\KeyWords;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\V1\Defaults\Operations\IndexOperationRequest;
+use App\Http\Requests\Api\V1\Defaults\Operations\ShowOperationRequest;
 use App\Http\Resources\Api\v1\Defaults\OperationResource;
 use App\Http\Resources\Api\v1\Defaults\TemplateResource;
 use App\Models\Defaults\Operation;
@@ -17,12 +19,9 @@ class OperationController extends Controller
      *
      * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
-    public function index(Request $request)
+    public function index(IndexOperationRequest $request)
     {
-        $data = $request->validate([
-            KeyWords::FILTER => 'array',
-            KeyWords::INCLUDE => 'array'
-        ]);
+        $data = $request->validated();
         $query = Operation::queryRequest($data, KeyWords::FILTER,  KeyWords::INCLUDE);
         return OperationResource::collection($query->get());
     }
@@ -33,9 +32,10 @@ class OperationController extends Controller
      * @param string $id
      * @return OperationResource
      */
-    public function show(Request $request, int $id)
+    public function show(ShowOperationRequest $request, int $id)
     {
-        $operation = Operation::queryRequest($request, KeyWords::INCLUDE)->find($id);
+        $data = $request->validated();
+        $operation = Operation::queryRequest($data, KeyWords::INCLUDE)->find($id);
         if(!$operation) abort(404);
         return new OperationResource($operation);
     }
---
+++ admin_api/app/Http/Requests/Api/V1/Defaults/Operations/IndexOperationRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Api\V1\Defaults\Operations;
+
+use App\CustomPackages\QueryRequest\KeyWords;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class IndexOperationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            KeyWords::FILTER => 'array:name',
+            KeyWords::INCLUDE => ['array', Rule::in(['categories', 'templates'])]
+        ];
+    }
+}
