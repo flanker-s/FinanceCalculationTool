661ceda1e021e35d35b7636ee2a38557c10467fe
Mohavk2 <mohavkv12@gmail.com>
2022-06-09 20:40:47 +0300

    Refactor. Replace template form select with an existed component

--- admin_ui/src/components/model/ClientResources/Templates/TemplateForm.jsx
+++ admin_ui/src/components/model/ClientResources/Templates/TemplateForm.jsx
@@ -5,15 +5,12 @@ import {
     DialogContent,
     DialogContentText,
     DialogTitle,
-    FormControl,
-    InputLabel,
-    Select, TextField
+    Stack,
+    TextField
 } from "@mui/material"
 import Button from "@mui/material/Button"
-import MenuItem from "@mui/material/MenuItem"
-import useApiResource from "../../../../api/useApiResource"
-import Loading from "../../../../pages/Loading/Loading"
 import React from "react"
+import ResourceSelect from "../../ResourceSelect"
 
 function TemplateForm({
                           title,
@@ -25,14 +22,7 @@ function TemplateForm({
                           handleAccept
                       }
 ) {
-    const {status, items} = useApiResource(
-        '/client_resources/categories',
-        {
-            filter: {
-                operation_id: operationId
-            }
-        }
-    )
+
     const {data, changeProperties} = useFormData(
         {
             name: initTemplateName,
@@ -40,53 +30,53 @@ function TemplateForm({
         }
     )
 
+    const handleCategorySelect = (id) => {
+        changeProperties({category_id: id})
+    }
+
     const accept = () => {
         handleAccept(data)
         handleClose()
     }
-    switch (status) {
-        case 'processing':
-            return <Loading/>
-        case 'completed':
-            return (
-                <Dialog open={true} onClose={handleClose}>
-                    <DialogTitle>{title}</DialogTitle>
-                    <DialogContent>
-                        <DialogContentText>
-                            {id ? 'ID: #' + id : ''}
-                        </DialogContentText>
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            label="Name"
-                            type="text"
-                            fullWidth
-                            variant="standard"
-                            value={data.name}
-                            onChange={e=>changeProperties({name: e.target.value})}
-                        />
-                        <FormControl variant="standard" fullWidth>
-                            <InputLabel>Category</InputLabel>
-                            <Select
-                                labelId="demo-simple-select-label"
-                                id="demo-simple-select"
-                                label="Age"
-                                value={data.category_id}
-                                onChange={e=>changeProperties({category_id: e.target.value})}
-                            >
-                                {items.map((item, i) => {
-                                    return <MenuItem key={i} value={item.id}>{item.attributes.name}</MenuItem>
-                                })}
-                            </Select>
-                        </FormControl>
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={handleClose}>Cancel</Button>
-                        <Button onClick={() => accept()}>Accept</Button>
-                    </DialogActions>
-                </Dialog>
-            )
-    }
+
+    return (
+        <Dialog open={true} onClose={handleClose}>
+            <DialogTitle>{title}</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    {id ? 'ID: #' + id : ''}
+                </DialogContentText>
+                <Stack spacing={1}>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="Name"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        value={data.name}
+                        onChange={e => changeProperties({name: e.target.value})}
+                    />
+                    <ResourceSelect
+                        url="/client_resources/categories"
+                        label="category"
+                        initQuery={{
+                            filter: {
+                                operation_id: operationId
+                            },
+                        }}
+                        initSelected={initCategoryId}
+                        selectHandler={handleCategorySelect}
+                        fullWidth
+                    />
+                </Stack>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={() => accept()}>Accept</Button>
+            </DialogActions>
+        </Dialog>
+    )
 }
 
 export default TemplateForm
